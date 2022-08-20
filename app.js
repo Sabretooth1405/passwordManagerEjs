0d3d96edd580b112e0117217156f70053fd05fd8
sabretooth1405 <sabretooth1405@gmail.com>
2022-08-20 17:11:13 +0530

    Added update features

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ async function main() {
 }
 let username = "";
 let passwd = "";
+let smallText="";
+let smallText2 = "";
 const passwdSchema=new mongoose.Schema({
     website:String,
     username:String,
@@ -44,9 +46,10 @@ app.get('/signup.html', function (req, res) {
 app.post('/signup.html', function (req, res) {
     username = req.body.username;
     passwd = req.body.passwd;
+    console.log(username);
     let rPaswd = req.body.rPasswd;
     if (passwd === rPaswd) {
-        User.find({ name: username }, function (err, doc) {
+        User.find({ username: username }, function (err, doc) {
             if (err) {
                 console.log(err);
                 res.redirect('back');
@@ -54,6 +57,7 @@ app.post('/signup.html', function (req, res) {
             }
             else if (doc.length !== 0) {
                 console.log("Username is already taken");
+                //console.log(doc);
                 res.redirect('back');
 
             }
@@ -95,8 +99,45 @@ app.get('/:customUrlName',function(req,res){
  const user=req.params.customUrlName;
  //console.log(user);
     console.log(req.params.customUrlName);
- res.render('user',{UserName:user});
+ res.render('user',{UserName:user,smallText:smallText});
+ smallText="";
 });
+app.get('/update/:user',function(req,res){
+    const user=req.params.user;
+   res.render('update',{UserName:user,smallText2:smallText2});
+});
+app.post('/update/:user', function (req, res) {
+    const user = req.params.user;
+    const website=req.body.website;
+    const password=req.body.passwd;
+    const rPasswd=req.body.rPasswd;
+    if(password===rPasswd)
+    {  console.log(password);
+        Password.findOneAndUpdate({website:website},{password:password},function(err,doc)
+        {   
+            if(!err)
+            {
+                console.log('password updated');
+                //console.log(doc);
+            }
+            else(
+                console.log(err)
+            );
+        });
+        User.updateOne({username:user,"info.website":website},{"info.$.password":password},function(err,doc){
+            if(!err)
+            {
+                console.log("Password updated in user db");
+                
+            }
+        });
+    }
+    else{
+        smallText2="Entered Passwords do not match.Pls type again";
+        console.log("Website not found");
+    }
+});
+
 app.post('/:customUrlName',function(req,res){
     const user=req.params.customUrlName;
     const website=req.body.website;
@@ -108,15 +149,39 @@ app.post('/:customUrlName',function(req,res){
         password:newPasswd
 
     });
-    User.findOneAndUpdate({username:user},{$push:{info:addPasswd}},function(err,docs){
-        if(err)
+   User.findOne({username:user},function(err,doc){
+    if(!err)
+    {
+        isWebsiteNew=doc.info.every((elm)=>{
+            if(elm.website===website){
+                console.log("Website already exits.Go to update page to change password");
+                smallText = `Website password already exits.Go to <a href="/update/${user}">update</a> page to change password`;
+                
+                return false;
+                
+            }
+        else{
+            return true;
+        }});
+        if(isWebsiteNew)
         {
-            console.log(err);
+            User.findOneAndUpdate({ username: user }, { $push: { info: addPasswd } }, function (err, docs) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    console.log("entry added successfully");
+                    res.redirect("back");
+                }
+            });
+            addPasswd.save();
         }
         else{
-            console.log("entry added successfully");
+            res.redirect('back');
         }
-    });
+    }
+   });
+    
 });
 app.listen(3000, function () {
     console.log('Serving on port 3000...');
